a99cb7f2be54f93022dbc83ad62c899d70155f91
Mike Renfro <renfro@tntech.edu>
2023-04-22 22:44:43 -0500

    compact long shell line

--- ruby/puppet_deps.rb
+++ ruby/puppet_deps.rb
@@ -2,5 +2,7 @@
 # https://stackoverflow.com/a/25009495
 def install_dep(name, version, install_dir = nil)
     install_dir ||= '/etc/puppetlabs/code/modules'
-    "mkdir -p #{install_dir} && (puppet module list | grep #{name}) || puppet module install -v #{version} #{name}"
+    "mkdir -p #{install_dir} && " \
+    "(puppet module list | grep #{name}) || " \
+    "puppet module install -v #{version} #{name}"
 end
